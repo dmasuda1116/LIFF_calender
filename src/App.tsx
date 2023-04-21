2db67d9cb9ae528477e54faf2c71de0f624e20b0
dmasuda1116 <dmasuda1116@gmail.com>
2023-04-21 23:40:39 +0900

    no message

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { TextField, Button } from "@mui/material";
+import { TextField, Button, Select, MenuItem, InputLabel, FormControl } from "@mui/material";
 import liff from "@line/liff";
 import Stack from '@mui/material/Stack';
 import DeleteIcon from "@mui/icons-material/Delete";
@@ -9,8 +9,10 @@ const Form = () => {
   const [duration, setDuration] = useState("");
   const [freeTime, setFreeTime] = useState("");
   const [preparationTime, setPreparationTime] = useState("");
-  const [startActivityTime, setStartActivityTime] = useState("");
-  const [endActivityTime, setEndActivityTime] = useState("");
+  const [startActivityTimeHour, setStartActivityTimeHour] = useState("");
+  const [startActivityTimeMinute, setStartActivityTimeMinute] = useState("");
+  const [endActivityTimeHour, setEndActivityTimeHour] = useState("");
+  const [endActivityTimeMinute, setEndActivityTimeMinute] = useState("");
 
   // LIFF initialization
   useEffect(() => {
@@ -32,12 +34,15 @@ const Form = () => {
 
 
   const register = () => {
+    const startActivityTime = `${startActivityTimeHour}時${startActivityTimeMinute}分`;
+    const endActivityTime = `${endActivityTimeHour}時${endActivityTimeMinute}分`;
+  
     const message: string = `カレンダー確認
-期間: ${duration}日間
-空き時間: ${freeTime}
-移動、準備時間: ${preparationTime}
-1日の開始活動開始時間: ${startActivityTime}
-1日の活動終了時間: ${endActivityTime}`;
+  期間: ${duration}日間
+  空き時間: ${freeTime}
+  移動、準備時間: ${preparationTime}
+  1日の開始活動開始時間: ${startActivityTime}
+  1日の活動終了時間: ${endActivityTime}`;
     liff
       .sendMessages([
         {
@@ -56,44 +61,105 @@ const Form = () => {
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-
+  
     register();
-
+  
     // フォームをリセットします。
     setDuration("");
     setFreeTime("");
     setPreparationTime("");
-    setStartActivityTime("");
-    setEndActivityTime("");
+    setStartActivityTimeHour("");
+    setStartActivityTimeMinute("");
+    setEndActivityTimeHour("");
+    setEndActivityTimeMinute("");
   };
+  
+    return (
+      <form onSubmit={handleSubmit}>
+        <FormControl fullWidth>
+          <InputLabel>期間 (日数)</InputLabel>
+          <Select
+            value={duration}
+            onChange={(e) => setDuration(e.target.value)}
+          >
+            {Array.from({length: 6}, (_, i) => 5 * (i + 1)).map((value) => (
+              <MenuItem key={value} value={value}>{value}</MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+  
+        <FormControl fullWidth>
+          <InputLabel>空き時間</InputLabel>
+          <Select
+            value={freeTime}
+            onChange={(e) => setFreeTime(e.target.value)}
+          >
+            {["15分", "30分", "45分", "1時間", "1時間15分", "1時間30分", "1時間45分", "2時間", "2時間15分", "2時間30分", "2時間45分", "3時間"].map((value) => (
+              <MenuItem key={value} value={value}>{value}</MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+  
+        <FormControl fullWidth>
+          <InputLabel>移動、準備時間</InputLabel>
+          <Select
+            value={preparationTime}
+            onChange={(e) => setPreparationTime(e.target.value)}
+          >
+            {["15分", "30分", "45分", "1時間", "1時間15分", "1時間30分", "1時間45分", "2時間", "2時間15分", "2時間30分", "2時間45分", "3時間"].map((value) => (
+              <MenuItem key={value} value={value}>{value}</MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+  
+        <FormControl fullWidth>
+          <InputLabel>1日の開始活動開始時間 (時間)</InputLabel>
+          <Select
+            value={startActivityTimeHour}
+            onChange={(e) => setStartActivityTimeHour(e.target.value)}
+          >
+            {Array.from({length: 24}, (_, i) => i + 1).map((value) => (
+              <MenuItem key={value} value={value}>{value}</MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+  
+        <FormControl fullWidth>
+          <InputLabel>1日の開始活動開始時間 (分)</InputLabel>
+          <Select
+          value={startActivityTimeMinute}
+          onChange={(e) => setStartActivityTimeMinute(e.target.value)}
+        >
+          {Array.from({length: 12}, (_, i) => 5 * i).map((value) => (
+            <MenuItem key={value} value={value}>{value}</MenuItem>
+          ))}
+        </Select>
+      </FormControl>
 
-  return (
-    <form onSubmit={handleSubmit}>
-      <TextField
-        label="期間 (日数)"
-        value={duration}
-        onChange={(e) => setDuration(e.target.value)}
-      />
-      <TextField
-        label="空き時間 (○時間○分)"
-        value={freeTime}
-        onChange={(e) => setFreeTime(e.target.value)}
-      />
-      <TextField
-        label="移動、準備時間 (○時間○分)"
-        value={preparationTime}
-        onChange={(e) => setPreparationTime(e.target.value)}
-      />
-      <TextField
-        label="1日の開始活動開始時間 (○時○分)"
-        value={startActivityTime}
-        onChange={(e) => setStartActivityTime(e.target.value)}
-      />
-      <TextField
-        label="1日の開始活動終了時間 (○時○分)"
-        value={endActivityTime}
-        onChange={(e) => setEndActivityTime(e.target.value)}
-      />
+      <FormControl fullWidth>
+        <InputLabel>1日の開始活動終了時間 (時間)</InputLabel>
+        <Select
+          value={endActivityTimeHour}
+          onChange={(e) => setEndActivityTimeHour(e.target.value)}
+        >
+          {Array.from({length: 24}, (_, i) => i + 1).map((value) => (
+            <MenuItem key={value} value={value}>{value}</MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+
+      <FormControl fullWidth>
+        <InputLabel>1日の開始活動終了時間 (分)</InputLabel>
+        <Select
+          value={endActivityTimeMinute}
+          onChange={(e) => setEndActivityTimeMinute(e.target.value)}
+        >
+          {Array.from({length: 12}, (_, i) => 5 * i).map((value) => (
+            <MenuItem key={value} value={value}>{value}</MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+
       <Stack direction="row" spacing={2}>
         <Button onClick={close} variant="outlined" startIcon={<DeleteIcon />}>
           削除
